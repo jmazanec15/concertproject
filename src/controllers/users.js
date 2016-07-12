b8e4fccc09b2b3ef79b61d10608b34df0ddebb7d
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-07-12 10:17:02 -0500

    day 2 start

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,36 +1,62 @@
 var express   = require('express'),
     Base      = express.Router(),
     fs        = require('fs'),
-    mongoose  = require('mongoose'),
-    User   = require('../models/user');
+    User      = require('../models/user'),
+    bcrypt    = require('bcrypt');
 
-Base.route('/?')
+Base.route('/register')
   // GET /
   // -----
   // Render the login page
   .get(function(req, res, next) {
-    res.render('login', {
+    res.render('register', {
       // csrfToken: req.csrf()
     });
   })
   // POST /
   // ------
-  // Log the user in
+  // Add user account to user model
   .post(function(req, res, next) {
-    User.create({
-      username: req.body.username,
-      password: req.body.password
-    }, function(err, user) {
-      if (err) {
-        console.log(err)
-        res.send('err')
-      } else {
+    User.findOne({username: req.body.username}, function(err, user) {
+      if (err || !user) {
+        res.send('err');
+      } else if (req.body.username === user.username) {
+        console.log(user.username)
+        res.send('Already taken')
+      } else if (user) {
         console.log(user)
-        res.send('worked')
+        res.send('Welcome')
       }
-
     })
-  })  
+  })
+
+
+
+  Base.route('/login')
+  //GET /
+  //-----
+  //Render the login page
+  .get(function(req, res, next) {
+    res.render('login', {
+    });
+  })
+  //POST /
+  //-----
+  //Log the user in
+  .post(function(req, res, next) {
+    User.findOne({username: req.body.username}, function(err, user) {
+      if (err || !user) {
+        res.send('Could not find user');
+      } else if (req.body.password === user.password) {
+        console.log(req.body.password)
+        res.send('Welcome Back')
+
+      } else {
+        res.send('Wrong password!')
+      }
+    })
+  })
+
 
 
 module.exports = Base;
