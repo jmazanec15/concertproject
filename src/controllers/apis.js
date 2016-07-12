ac9a169e4f911f6b3bccd955e360767a900d2a09
Jack Mazanec <jmazane1@nd.edu>
2016-07-12 12:32:23 -0500

    apis update

--- src/controllers/apis.js
+++ src/controllers/apis.js
@@ -14,17 +14,32 @@ Apis.route('/')
 .post(function(req, res, next) {
 
   var artist = req.body.artist
-  var url = 'http://api.bandsintown.com/artists/';
-  // var url = 'http://api.bandsintown.com/artists/' + artist + '/events.json?api_version=2.0&app_id=projectGA';
+  // var url = 'http://api.bandsintown.com/artists/';
+  var url = 'http://api.bandsintown.com/artists/' + artist + '/events.json?api_version=2.0&app_id=projectGA';
   superagent.get(url, function(error, response){
     if (error) {
       res.render('apis');
     } else {
-    // var Obj = response.body[0].title;
+
     var Obj = response.body;
+    //console.log(Obj)
+    var eventsArray = [];
+    var datesArray = [];
     console.log(Obj);
-    // apis.append('<li>'+ Obj +'</li>');
-    res.render('apis');
+    for (var i = 0; i <= Obj.length - 1; i++) {
+      var date = Obj[i].datetime;
+      // 2016-10-08T20:00:00
+      var year = date[0] + date[1] + date[2] + date[3],
+          day  = date[5] + date[6],
+          month = date[8] + date[9]
+
+          date = day + '/' + month + '/' + year;
+      eventsArray.push(Obj[i].title + ' Date: ' + date);
+      
+    }
+
+     // + ' date: ' + datesArray
+    res.render('apis',{pageTitle: eventsArray});
     }
   });
   
