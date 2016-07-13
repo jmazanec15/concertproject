b8401e48766bc7265512accc113269634f101aec
Cyrus Goudarzi <cgoudarzi@gmail.com>
2016-07-13 10:37:51 -0500

    wed update

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -18,13 +18,13 @@ loginController.route('/?')
   .post(function(req, res, next) {
     User.findOne({username: req.body.username}, function(err, user) {
       if (err || !user) {
-        console.log(hash)
+        console.log(user)
         res.send('Error');
       } else {
         bcrypt.compare(req.body.password, user.password, function(error, result) {
           if (error || !result) {
             console.log(error)
-            res.send('Error or Incorrect password')
+            res.send('Error')
           } else {
             console.log(result)
             res.redirect('/apis')
